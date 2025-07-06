7249d901b428d39719e84d1a4e8eb7aa0cb15507
Riteshkumrsoni <riteshsoni062022@gmail.com>
2025-07-06 10:36:54 +0530

    Modifying env file

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
